a8820e2457c2d17f185f552e24117d3e08e336b1
Christopher N. KATOYI-KABA <christopher.katoyi@icloud.com>
2017-07-29 12:04:08 +0200

    Firebase init

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import Firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/loginForm';
 
 class App extends Component {
+
+    componentWillMount() {
+        Firebase.initializeApp({
+            apiKey: 'AIzaSyC9Paf72HLrgwwv1_pKaT5EtXMbbpEAnx0',
+            authDomain: 'auth-react-native-fabad.firebaseapp.com',
+            databaseURL: 'https://auth-react-native-fabad.firebaseio.com',
+            projectId: 'auth-react-native-fabad',
+            storageBucket: 'auth-react-native-fabad.appspot.com',
+            messagingSenderId: '261318109506'
+        });
+    }
+
+
     render() {
         return (
             <View>
                 <Header title='Authentication' />
-                <Text>Hello</Text>
+                <LoginForm />
             </View>
         );
     }
